692c12a5cc021bfcb601b0264682fe6eef1297df
MASTERB76 <87577464+MASTERB76@users.noreply.github.com>
2021-11-05 16:29:59 +0700

    Add files via upload

---
+++ Cyber-Bagus.sh
@@ -0,0 +1,75 @@
+jernih 
+bi='\033[34;1m' #biru 
+i='\033[32;1m' #ijo 
+pur='\033[35;1m' #ungu cy='\033[36;1m' #cyan me='\033[31;1m' #merah 
+pu='\033[37;1m' #putih ku='\033[33;1m' #kuning mer='\033[41;97m' #Tepi 
+st='\033[0m' #Berhenti 
+R='\x1b[1;31m' 
+G='\x1b[1;32m' 
+B='\x1b[1;34m' 
+Y='\x1b[1;33m' 
+C='\x1b[1;36m' 
+D='\x1b[0m
+
+#Mod oleh : Cyber Bagus
+#Alat Unfaedah : Hehe;"v"
+
+jernih
+
+echo $pur" "
+ echo $pur" " 
+ echo $pur" " 
+ echo $mer" ×××Bagus patc | BagusXploit $st"
+ 
+ lagi=1
+  while [ $lagi -lt 6 ]; 
+  melakukan
+ gema"" 
+ echo $cy"1. Sadap WA Kerja Ni Bep :v "; 
+ echo $pur"====================]" echo $cy"2. Retas Satelit";
+  echo $pur"====================]" echo $cy"3. Hack Listrik"; 
+  echo $pur"====================]" echo $cy"4. Ngepet Online"; 
+  echo $pur"====================]" echo $cy"5. Ngewe Online [Jangan Di Pake Om]:v"; 
+  echo $pur"====================]" echo $cy"6. Santet Online";
+   echo $pur"====================]" echo $i"7. KELUAR $st"; gema "" 
+   echo $mer"╭─Pilih Nomor Berapa Bep!!? $st " baca -p "╰─•> " pil; 
+
+kasus $pil in 
+1) jelas 
+gema $i"
+████╗ █████╗ ██████╗ █████╗ ██████╗ ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗ ███████╗███████║██║ ██║███████║██████╔╝ ╚════██║██╔══██║██║ ██║██╔══██║██╔═══╝ ███████║██║ ██║██████╔╝██║ ██║██║ ╚══════╝╚═╝ ╚═╝╚═════╝ ╚═╝ ╚═╝╚═╝
+
+"
+gema ""
+echo -n $cy"Ini Isi Nomor Target:" baca tr echo "============================" | lolcat echo -n $cy"Nah ini isi nomor lu : " baca lu jernih echo "Sabar Bep lagi nyadap ini $tr" | lolcat gema "" echo $mer " Tunggu Sekitar 3menit ya Bep $st" gema "" tidur 50 echo $cy " Sedang Mengambil Chat Terbaru Om" tidur 4 echo $bi " Chat Terbaru Dengan 'My Honey' " echo $cy " <Kemarin> " gema "" echo $pu " Sayangku: Bep $cy[09:22]" echo $i " Saya : Apaan Apa? $cy[09:22]" echo $pu " My Honey : Besok Masuk Jam Brp? $cy[09:23]" echo $i " Me : Ya seperti biasa jam 7 $cy[09.23]" echo $pu " Sayangku : Bareng kita berangkat ya, gua jemput lu besok $cy[09.24]" echo $i " Saya : Gua males dah sekolah besok, dirumah ga ada org ortu gua pergi besok $cy[09:24]" echo $pu " Sayangku : Ya udh gak usah masuk kita besok $cy[09:25]" echo $i " Saya : Ashiiiaaaap.. Utama dirumah gua aja besok $cy[09:26]" echo $pu " Sayangku : Oke Oke $cy[09:36]" echo $pu " Sayangku : Eh VCS kuy.. $cy[15:06]" echo $i " Me : Hmm.. kuy lah.. lagi bosen gua $cy[15:06]" echo $pu " Sayangku : Oke dah, gua mau off, besok gua kerumah lu $cy[17:05]" echo $i " Me : Ok.. Besok kita senang-senang.. $cy[17:22]" echo $pu " Sayangku : Wkwk.. ashiapp.. Lope you Babi🤣😘😘 $cy[17:22]" keluar 
+;;
+
+2) jelas
+sh 2.sh
+keluar
+;;
+
+2)jelas
+sh 3.sh
+keluar
+;;
+
+4) jelas
+sh 4.sh
+keluar
+;;
+
+5) jelas
+sh 5.sh
+keluar
+;;
+
+
+6) jelas
+ sh 6.sh 
+ keluar
+ ;;
+ 
+7) echo $cy " Sampai jumpa Bep  Yekan" keluar ;; *) echo "Selesay" | lolcat esac selesai
+ selesai
+ keluar
